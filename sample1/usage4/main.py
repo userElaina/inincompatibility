f4cebba3d08cd17e35dce8c6a786c6cc5f9c08c4
elaina <userelaina@pm.me>
2024-12-10 13:51:21 +0000

    :sparkles: sample1.usage4

--- sample1/usage4/main.py
+++ sample1/usage4/main.py
@@ -2,7 +2,7 @@ import torch
 from torch import Tensor
 from to_import import getdata, calc
 
-assert str(torch.__version__).startswith('2.5')
+assert str(torch.__version__).startswith('1.')
 
 print(getdata(0))
 print(calc(Tensor([1.0, 2.0, 3.0]), Tensor([0.1, 0.2, 0.3])))
