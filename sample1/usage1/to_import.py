055a52dd5cd11127d43894bbae49cee8ccf61c78
userElaina <userelaina@pm.me>
2024-12-10 10:46:16 +0000

    :truck: `_func_eval`

--- sample1/usage1/to_import.py
+++ sample1/usage1/to_import.py
@@ -2,17 +2,19 @@ import socket
 import pickle
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 13587))
+client.connect(('127.0.0.1', 2983))
 
 BUFFER_SIZE = 4096
 
-def calc(*args, **kwargs):
-    data = pickle.dumps(('calc', args, kwargs))
+def _func_eval(func, args, kwargs):
+    data = pickle.dumps((func, args, kwargs))
     client.sendall(data)
     return pickle.loads(client.recv(BUFFER_SIZE))
+
+
+def calc(*args, **kwargs):
+    return _func_eval('calc', args, kwargs)
 
 
 def getdata(*args, **kwargs):
-    data = pickle.dumps(('getdata', args, kwargs))
-    client.sendall(data)
-    return pickle.loads(client.recv(BUFFER_SIZE))
+    return _func_eval('getdata', args, kwargs)
