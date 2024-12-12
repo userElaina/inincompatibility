406347eff45d109e07ba3d75a4c6d401e3ed2967
elaina <userelaina@pm.me>
2024-12-12 12:33:02 +0000

    :white_check_mark: sample1.usage3

--- sample1/usage3/server_lib1.py
+++ sample1/usage3/server_lib1.py
@@ -1,7 +1,10 @@
-from lib1 import getdata, calc
+from lib1 import getdata, calc, _inincompatibility_client_connect_callback
+from lib1 import _inincompatibility_client_close_callback
 from inincompatibility import IServer
 
-iics = IServer(debug=True)
-iics.add_funcs(getdata, calc)
-iics.gen_import_code()
-iics.run()
+inincs = IServer(verbose=True)
+inincs.add_funcs(getdata, calc)
+inincs.client_connect_callback = _inincompatibility_client_connect_callback
+inincs.client_close_callback = _inincompatibility_client_close_callback
+inincs.gen_import_code()
+inincs.run()
--- sample1/usage3/to_import.py
+++ sample1/usage3/to_import.py
@@ -5,7 +5,7 @@ import socket
 import pickle
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 55405))
+client.connect(('127.0.0.1', 43987))
 
 BUFFER_SIZE = 4096
 
@@ -16,12 +16,12 @@ def _func_eval(func, args, kwargs):
     return pickle.loads(client.recv(BUFFER_SIZE))
 
 
-def remote_eval(*args, **kwargs):
-    return _func_eval("remote_eval", args, kwargs)
+def _inincompatibility_remote_eval(*args, **kwargs):
+    return _func_eval("_inincompatibility_remote_eval", args, kwargs)
 
 
-def remote_exec(*args, **kwargs):
-    return _func_eval("remote_exec", args, kwargs)
+def _inincompatibility_remote_exec(*args, **kwargs):
+    return _func_eval("_inincompatibility_remote_exec", args, kwargs)
 
 
 def getdata(*args, **kwargs):
