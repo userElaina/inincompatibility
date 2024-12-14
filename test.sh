5b5963d64cb2d4777f4fc89d74430e88516d7598
elaina <userelaina@pm.me>
2024-12-14 10:36:15 +0000

    :white_check_mark: update `test.sh`

--- test.sh
+++ test.sh
@@ -6,34 +6,45 @@ conda activate py37
 conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 cpuonly -c pytorch
 
 conda deactivate
-conda deactivate
 py -m build
 twine check dist/*
 twine upload --repository testpypi dist/* --verbose
 
+diff sample1/ori/main.py sample1/usage0/main.py
+diff sample1/ori/main.py sample1/usage1/main.py
+diff sample1/ori/main.py sample1/usage2/main.py
+diff sample1/ori/main.py sample1/usage3/main.py
+diff sample1/ori/lib1.py sample1/usage0/lib1.py
+diff sample1/ori/lib1.py sample1/usage1/lib1.py
+diff sample1/ori/lib1.py sample1/usage2/lib1.py
+diff sample1/ori/lib1.py sample1/usage3/lib1.py
+
 conda activate py311
 conda list | grep torch
 pip install --upgrade -i https://test.pypi.org/simple/ inincompatibility
-cd sample1/usage2
+cd sample1/usage0
 py server_lib1.py
-cd ../usage3
+cd ../usage1
 py server_lib1.py
-cd ../usage4
+cd ../usage2
 py -m inincompatibility -i to_import_ori.py -o to_import.py --buffersize 8192 -a "0.0.0.0:0" --verbose
+cd ../usage3
+py -m inincompatibility -i "from lib1 import getdata, calc, _inincompatibility_client_connect_callback; from lib1 import _inincompatibility_client_close_callback;" -o to_import.py --buffersize 8192 -a "0.0.0.0:0" --verbose --inputcode
 
 conda activate py37
 conda list | grep torch
 pip install --upgrade -i https://test.pypi.org/simple/ inincompatibility
 cd sample1/ori
 py main.py
+cd ../usage0
+py main.py
+cd ../usage1
+py main.py
 cd ../usage2
 py main.py
 cd ../usage3
 py main.py
-cd ../usage4
-py main.py
 
-conda deactivate
 conda deactivate
 py -m build
 twine check dist/*
