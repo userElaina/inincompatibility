7eb2516ec3bde32cec1ebed8df2009dcfa2c3a8a
elaina <userelaina@pm.me>
2024-12-16 12:03:37 +0000

    :fire: sample1.usage2

--- sample1/usage2/run.sh
+++
@@ -1,7 +0,0 @@
-conda activate py311
-conda list | grep torch
-py -m inincompatibility -i to_import_ori.py -o to_import.py --buffersize 8192 -a "0.0.0.0:0" --verbose
-
-conda activate py37
-conda list | grep torch
-python main.py
--- sample1/usage2/to_import_ori.py
+++
@@ -1,2 +0,0 @@
-from lib1 import getdata, calc, _inincompatibility_client_connect_callback
-from lib1 import _inincompatibility_client_close_callback
