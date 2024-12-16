da20d9d51177d36a2fed5ec7d6f2cf883d41b77e
elaina <userelaina@pm.me>
2024-12-16 11:45:31 +0000

    :white_check_mark: LLaMA sample

--- sample-llama/main.py
+++ sample-llama/main.py
@@ -1,12 +1,4 @@
 import torch
 from to_import import llm_qa
-
-assert str(torch.__version__).startswith('1.')
-
-msg = [
-    {"role": "system", "content": "You are a cat girl who speaks in a very gentle tone and says Meow at the end!"},
-    {"role": "user", "content": "Who are you?"},
-]
-
-res = llm_qa(msg)
-print(res["content"])
+assert torch.__version__.startswith('1')
+print(llm_qa('The key to life is'))
--- sample-llama/your_llm.py
+++ sample-llama/your_llm.py
@@ -1,19 +1,13 @@
 import torch
-import transformers
-
-assert str(torch.__version__).startswith('2.')
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model="meta-llama/Meta-Llama-3.1-8B-Instruct",
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device="cuda"
-)
+from transformers import pipeline as pl
+assert torch.__version__.startswith('2')
+m = "meta-llama/Llama-3.2-1B"
+p = pl("text-generation", model=m, device="cuda")
 
 
 def llm_qa(msg: list) -> dict:
-    res = pipeline(
-        msg,
-        max_new_tokens=512,
-    )
-    return res[0]["generated_text"][-1]
+    return p(msg)[0]["generated_text"]
+
+
+if __name__ == '__main__':
+    print(llm_qa('The key to life is'))
